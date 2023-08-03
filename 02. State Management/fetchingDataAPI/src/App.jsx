69762276c12f77927d2e25d24a659c4e89c933fc
averma-frontrol <averma@frontrol.com>
2023-08-03 11:54:23 +0530

    fetchAPIData

---
+++ 02. State Management/fetchingDataAPI/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+// eslint-disable-next-line react/prop-types
+function App({ id }) {
+  const [product, setProduct] = useState();
+
+  // impure code
+  async function fetchData(id) {
+    const res = await fetch(`https://dummyjson.com/products/${id}`);
+    const data = await res.json();
+    setProduct(data);
+  }
+
+  //safe place to write impure code.
+  useEffect(() => {
+    fetchData(id);
+  }, [id]);
+  return (
+    <>
+      <h1>{product.brand}</h1>
+      <p>{product.title}</p>
+      <img src={product.images[0]} alt={product.title} />
+      <p>Rs. {product.price}</p>
+    </>
+  );
+}
+
+export default App;
